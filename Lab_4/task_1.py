991d2906b59fca73d78076bc8efe1746a9727332
Akhmed04 <vubazaakhmed@gmail.com>
2025-03-12 16:30:49 +0600

    OOP lab_4
    были исправлены ошибки и добавлена документация. Прошу прощения за задержку

--- Lab_4/task_1.py
+++ Lab_4/task_1.py
@@ -1,8 +1,23 @@
 """"родительский класс социальные сети"""
 class SocialNetwork:
     def __init__(self, name: str, users: int):
-        self.name = name
-        self.users = users
+        """
+        Инициализация объекта социальная сеть
+        :param name: Название социальной сети
+        :param users: Количество пользователей в социальной сети
+        """
+        self._name = name
+        self._users = users
+
+    @property
+    def name(self) -> str:
+        """Возвращает название"""
+        return self._name
+
+    @property
+    def users(self) -> int:
+        """Возврашает количество пользователей"""
+        return self._users
 
     def __str__(self) -> str:
         return f"Социальная сеть: {self.name}, Кол-во пользователей: {self.users}"
@@ -12,25 +27,56 @@ class SocialNetwork:
 
     """""Добавление пользователей"""
     def add_users(self, count: int):
-        self.users +=count
+        """
+        Добавляет пользователей в социальную сеть
+        :param count: Количество новых пользователей
+        """
+        self._users +=count
+
 """"дочерний класс телеграм"""
 class Telegram(SocialNetwork):
     def __init__(self, users: int, groups: int):
+        """
+        Инициализация обьекта Телеграм
+        :param users: Количество пользователей в телеграме
+        :param groups: Количество групп
+        """
         super().__init__(name='Telegram', users=users)
-        self.groups = groups
+        self._groups = groups
+
+    @property
+    def groups(self) -> int:
+        """Возвращает количество групп"""
+        return self._groups
 
     def __str__(self) ->str:
         return f"Телеграм: Кол-во пользователей: {self.users}, Групп: {self.groups}"
 
     def __repr__(self) ->str:
-        return f"Телеграм(Кол-во пользователей: {self.users}, Групп: {self.groups})"
+        return f"{self.__class__.__name__} (Кол-во пользователей: {self.users}, Групп: {self.groups})"
 
     def add_users(self, count: int):
+        """
+        Добавляет пользователей и группы (на 100 человек 1 группа)
+        :param count: Количество новых пользователей
+        """
         super().add_users(count)
-        self.groups =+ count // 1000
+        self._groups += count // 100
 
 
 
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+
+    instagram = SocialNetwork("Instagram", 100000000)
+    print(instagram)
+    print(repr(instagram))
+
+    instagram.add_users(2000000)
+    print(instagram)
+
+
+    telegram = Telegram(10000,50)
+    print(telegram)
+    print(repr(telegram))
+    telegram.add_users(5000)
+    print(telegram)
